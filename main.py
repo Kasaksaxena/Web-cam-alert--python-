d73d777a545956760c77a58890746b89354a83c3
Kasaksaxena <kasaksaxena6@gmail.com>
2024-12-31 22:55:01 +0530

    extract images from web cam video

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import cv2
 import time
+import glob
 from emailing import send_email
 video=cv2.VideoCapture(0)
 time.sleep(1)
 first_frame=None
 status_list=[]
+count=1
 
 while True:
     status=0
+    
     check,frame=video.read()
     
     #converts frame from color(BGR) to grayscale
@@ -39,7 +42,7 @@ while True:
     #filter smaller contours
     #skips contour smaller than 1000 to avoid noises 
     for contour in contours:
-        if cv2.contourArea(contour)<8000:
+        if cv2.contourArea(contour)<5000:
             continue
         
         #cal bounding rectangle
@@ -48,9 +51,15 @@ while True:
         if rectangle.any():
             status=1 #indicate if motion is detected
             
+            cv2.imwrite(f"images/{count}.png",frame)
+            count=count+1
+            all_images=glob.glob("images/*.png")
+            index=int(len(all_images)/2)
+            image_with_object=all_images[index]
+            
     status_list.append(status) 
     status_list=status_list[-2:] # keeps only two recent statuses to avoid memory overload
-    
+    print(status_list)
     # transition (1 to 0) indicates motion stopped and send email func triggered
     if status_list[0]==1 and status_list[1]==0:
         send_email()
